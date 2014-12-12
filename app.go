5ad45af9b0d000e7dbd4b9addb9801815e1b77a9
Christopher Probst <christopher.probst@uni-duesseldorf.de>
2014-12-12 10:29:50 +0100

    Parallelize everything

--- app.go
+++ app.go
@@ -1,53 +1,61 @@
 package main
 
 import (
-	"flag"
 	"image"
 	"image/color"
 	"image/jpeg"
 	"log"
 	"os"
 	"path/filepath"
+	"runtime"
+	"sync"
 )
 
 func main() {
-	pathPtr := flag.String("p", "", "Path to image to invert")
-	flag.Parse()
-	path, err := filepath.Abs(*pathPtr)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Println(path)
+	runtime.GOMAXPROCS(4)
+	var group sync.WaitGroup
+	for _, arg := range os.Args[1:] {
+		group.Add(1)
+		go func() {
+			defer group.Done()
+			path, err := filepath.Abs(arg)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			log.Println(path)
 
-	f, err := os.Open(path)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Println("Opened file")
+			f, err := os.Open(path)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			log.Println("Opened file")
 
-	img, err := jpeg.Decode(f)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Println("Decoded image file")
+			img, err := jpeg.Decode(f)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			log.Println("Decoded image file")
 
-	newImg := image.NewRGBA(img.Bounds())
-	for x := 0; x < img.Bounds().Dx(); x++ {
-		for y := 0; y < img.Bounds().Dy(); y++ {
-			r, g, b, _ := img.At(x, y).RGBA()
-			r = 255 - r
-			g = 255 - g
-			b = 255 - b
+			newImg := image.NewRGBA(img.Bounds())
+			for x := 0; x < img.Bounds().Dx(); x++ {
+				for y := 0; y < img.Bounds().Dy(); y++ {
+					r, g, b, _ := img.At(x, y).RGBA()
+					r = 255 - r
+					g = 255 - g
+					b = 255 - b
 
-			newImg.Set(x, y, color.RGBA{uint8(r), uint8(g), uint8(b), 255})
-		}
-	}
+					newImg.Set(x, y, color.RGBA{uint8(r), uint8(g), uint8(b), 255})
+				}
+			}
 
-	fout, err := os.Create(path + "_inverted.jpg")
-	if err != nil {
-		log.Fatalln(err)
+			fout, err := os.Create(path + "_inverted.jpg")
+			if err != nil {
+				log.Fatalln(err)
+			}
+			if err := jpeg.Encode(fout, newImg, nil); err != nil {
+				log.Fatalln(err)
+			}
+		}()
 	}
-	if err := jpeg.Encode(fout, newImg, nil); err != nil {
-		log.Fatalln(err)
-	}
+	group.Wait()
 }
